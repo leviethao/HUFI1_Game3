498dbf04c605209a707a6bc53889a56e1feb4754
leviethao <leviethao2@gmail.com>
2018-05-22 02:03:09 -0700

    hao complete enemy

--- assets/scripts/EnemyRotateOrigin.ts
+++ assets/scripts/EnemyRotateOrigin.ts
@@ -9,44 +9,18 @@
 //  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
 
 const {ccclass, property} = cc._decorator;
-import InGame from "./InGame";
+
 @ccclass
 export default class NewClass extends cc.Component {
 
-    colors: cc.Color[] = [];
-    colorIndex: number;
-
-    inGame: InGame = null;
     // LIFE-CYCLE CALLBACKS:
 
-    onLoad () {
-        this.colors = [cc.Color.RED, cc.Color.BLUE, cc.Color.GREEN, cc.Color.YELLOW, cc.Color.WHITE, cc.Color.ORANGE, cc.Color.MAGENTA];
-        this.colorIndex = 0;
-    }
+    onLoad () {}
 
     start () {
-        this.schedule(this.changeColor, 1, cc.macro.REPEAT_FOREVER);
+        let rotateAction = cc.rotateBy(0.2, 30);
+        this.node.runAction(cc.repeatForever(rotateAction));
     }
 
-    update (dt) {}
-
-    init () {
-        this.inGame.enemyContainer.push(this.node);
-    }
-
-    destroyEnemy () {
-        this.inGame.enemyContainer.splice(this.inGame.enemyContainer.indexOf(this.node), 1);
-    }
-
-    changeColor () {
-        this.colorIndex = (this.colorIndex + 1) % this.colors.length;
-        this.node.color = this.colors[this.colorIndex];
-    }
-
-    reuse () {
-    }
-
-    unuse () {
-
-    }
+    // update (dt) {}
 }
--- assets/scripts/InGame.ts
+++ assets/scripts/InGame.ts
@@ -9,23 +9,84 @@
 //  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
 
 const {ccclass, property} = cc._decorator;
+const DEFAULT_ENEMY_POOL_SIZE = 10;
+import Enemy from "./Enemy";
 
 @ccclass
 export default class NewClass extends cc.Component {
 
-    @property(cc.Label)
-    label: cc.Label = null;
+    @property(cc.Node)
+    enemyRotateOrigin: cc.Node = null;
 
-    @property
-    text: string = 'hello';
+    @property(cc.Node)
+    circle: cc.Node = null;
 
+    @property(cc.Prefab)
+    enemyPrefab: cc.Prefab = null;
+
+    enemyPool: cc.NodePool = null;
+    trajectoryRadius: number = 0;
+    enemyContainer: cc.Node[] = [];
     // LIFE-CYCLE CALLBACKS:
 
-    // onLoad () {}
+    onLoad () {
+        this.enemyPool = new cc.NodePool("Enemy");
+        for (let i = 0; i < DEFAULT_ENEMY_POOL_SIZE; i++) {
+            let enemy = cc.instantiate(this.enemyPrefab);
+            this.enemyPool.put(enemy);
+        }
 
-    start () {
-
+        this.enemyRotateOrigin.position = this.circle.position;
+        this.trajectoryRadius = this.circle.width * 1.2;
     }
 
-    // update (dt) {}
+    start () {
+        for (let i = 0; i < 15; i++) {
+            this.spawnEnemy();
+        }
+    }
+
+    update (dt) {}
+
+    createEnemy () : cc.Node {
+        let enemy: cc.Node = null;
+        if (this.enemyPool.size() > 0) {
+            enemy = this.enemyPool.get();
+        } else {
+            enemy = cc.instantiate(this.enemyPrefab);
+        }
+        enemy.getComponent(Enemy).inGame = this;
+        return enemy;
+    }
+
+    spawnEnemy () {
+        let enemy = this.createEnemy();
+        this.enemyRotateOrigin.addChild(enemy);
+
+        //random position
+        let angleRand = Math.floor(cc.randomMinus1To1() * 180);
+        let vec1 = new cc.Vec2(1, 0);
+        let vec2 = cc.Vec2.ZERO;
+
+        do {
+            vec2.x = vec1.x / Math.cos(angleRand);
+            vec2.y = Math.tan(angleRand) * vec1.x;
+            vec2.normalizeSelf();
+            
+            vec2.x = vec2.x * this.trajectoryRadius + this.circle.x;
+            vec2.y = vec2.y * this.trajectoryRadius +  this.circle.y;
+        } while (this.checkOverrideEnemy(vec2));
+        
+
+        enemy.position = vec2;
+    }
+
+    checkOverrideEnemy(vec: cc.Vec2) {
+        for (let enemy of this.enemyContainer) {
+            if (vec.sub(enemy.position).mag() > enemy.width) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
